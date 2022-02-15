4de76819a88c934c3c6103ce46a48b63fceced9b
Gabriel Carvalho <gabrielbdec@gmail.com>
2022-02-15 20:35:44 -0300

    feat: initial commit

---
+++ src/pages/sigin/sigin.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+
+export const Home = () => {
+  return (
+    <div>Home works!</div>
+    // Code here
+  );
+};
---
+++ src/shared/validators/email.validator.ts
@@ -0,0 +1,6 @@
+export default class emailValidator {
+  public isValid(): boolean {
+    // Code here
+    return true;
+  }
+}
