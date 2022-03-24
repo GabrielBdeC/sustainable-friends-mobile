3da0fc1185e8777920631d148bb4396419bf6053
GabrielBdec <gabrielbdec@gmail.com>
2022-03-24 00:10:09 -0300

    feat: map

--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -1,9 +1,85 @@
-import React from "react";
+import React, { useState } from 'react'
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import "./map.css";
 
-export const Map = () => {
-  return (
-    <div>Map works!</div>
-    // Code here
-  );
+const containerStyle = {
+  width: '1870px',
+  height: '950px'
 };
+
+function onMarkerClick() {
+}
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDQISO3k52QEUZ_tQ3zsM6gbX5p4UlvirI"
+  })
+
+  const [position, setMovement] = useState({lat: 0, lng: 0});
+
+  let idWatch: number;
+
+  const onLoad = React.useCallback((map) => {
+    navigator.geolocation.getCurrentPosition((resp)=>{
+      setMovement({
+        lat: resp.coords.latitude,
+        lng: resp.coords.longitude,
+      });
+    });
+    idWatch = navigator.geolocation.watchPosition(()=>{
+      navigator.geolocation.getCurrentPosition((resp)=>{
+        setMovement({
+          lat: resp.coords.latitude,
+          lng: resp.coords.longitude,
+        });
+      });
+    });
+  }, []);
+
+  return isLoaded ? (
+    <div className="container-map">
+      <GoogleMap
+        center={{ lat: position.lat, lng: position.lng }}
+        mapContainerStyle={containerStyle}
+        zoom={14}
+        onLoad={onLoad}
+      >
+        <Marker
+          position={{ lat: position.lat, lng: position.lng }}
+          icon={{
+            url: "https://imgkub.com/images/2022/03/20/pessoa_WB.png",
+            scaledSize: new google.maps.Size(30, 30)
+          }}
+        />
+        <Marker
+          onClick={onMarkerClick}
+          position={{ lat: -28.930480, lng: -49.472340 }}
+          icon={{
+            url: "https://imgkub.com/images/2022/03/20/coleta_WB.png",
+            scaledSize: new google.maps.Size(30, 30)
+          }}
+        />
+        <Marker
+          onClick={onMarkerClick}
+          position={{ lat: -28.930580, lng: -49.412940 }}
+          icon={{
+            url: "https://imgkub.com/images/2022/03/20/entrega_WB.png",
+            scaledSize: new google.maps.Size(30, 30)
+          }}
+        />
+        <Marker
+          onClick={onMarkerClick}
+          position={{ lat: -28.930510, lng: -49.432940 }}
+          icon={{
+            url: "https://imgkub.com/images/2022/03/20/entrega_WB.png",
+            scaledSize: new google.maps.Size(30, 30)
+          }}
+        />
+        <></>
+      </GoogleMap>
+    </div>
+  ) : <></>
+}
+
+export default React.memo(Map);
