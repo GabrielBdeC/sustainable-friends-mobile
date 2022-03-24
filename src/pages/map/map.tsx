d43f046514916e9dd78efdfff4c3996c7dbd1c2d
Régis Nyland Bloemer <regisnb1@gmail.com>
2022-03-24 20:05:44 -0300

    teste

--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -3,6 +3,7 @@ import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import "./map.css";
 import Button from "../../shared/components/button/button";
 
+
 function onMarkerClick() {
 } 
 
@@ -48,30 +49,7 @@ function Map() {
             scaledSize: new google.maps.Size(30, 30)
           }}
         />
-        <Marker
-          onClick={onMarkerClick}
-          position={{ lat: -28.930480, lng: -49.472340 }}
-          icon={{
-            url: "https://imgkub.com/images/2022/03/20/coleta_WB.png",
-            scaledSize: new google.maps.Size(30, 30)
-          }}
-        />
-        <Marker
-          onClick={onMarkerClick}
-          position={{ lat: -28.930580, lng: -49.412940 }}
-          icon={{
-            url: "https://imgkub.com/images/2022/03/20/entrega_WB.png",
-            scaledSize: new google.maps.Size(30, 30)
-          }}
-        />
-        <Marker
-          onClick={onMarkerClick}
-          position={{ lat: -28.930510, lng: -49.432940 }}
-          icon={{
-            url: "https://imgkub.com/images/2022/03/20/entrega_WB.png",
-            scaledSize: new google.maps.Size(30, 30)
-          }}
-        />
+          
         <></>
       </GoogleMap>
       <Button type="submit" buttonSize="btn--little" ><p className="p">Adicionar Ponto</p></Button>
@@ -79,6 +57,7 @@ function Map() {
     
   ) : <></>
 }
+
 
 export default React.memo(Map);
 
